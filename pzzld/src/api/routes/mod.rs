b9d4167a72ffbc88a8d16f47143b180cd7796331
Joe McCain III <jo3mccain@icloud.com>
2022-12-07 05:09:35 +0000

    Update

--- pzzld/src/api/routes/mod.rs
+++
@@ -1,9 +0,0 @@
-/*
-   Appellation: core <module>
-   Contrib: FL03 <jo3mccain@icloud.com>
-   Description: ... Summary ...
-*/
-pub use self::{auth::*, index::Homepage};
-
-pub(crate) mod auth;
-pub(crate) mod index;
