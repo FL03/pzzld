ca8081887bff57e0922909bee7fbf72e7176e57d
Joe McCain III <jo3mccain@icloud.com>
2022-12-28 17:28:32 +0000

    Update

--- crates/wallets/src/lang.rs
+++ crates/wallets/src/lang.rs
@@ -3,17 +3,10 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... Summary ...
 */
+use scsys::fnl_remove;
 use serde::{Deserialize, Serialize};
 use std::convert::From;
 use strum::{EnumString, EnumVariantNames};
-
-fn fnl_remove<T: Clone + ToString>(data: &T) -> String {
-    let data = data.to_string();
-    let mut chars = data.chars();
-    chars.next();
-    chars.next_back();
-    chars.as_str().to_string()
-}
 
 #[derive(
     Clone,
--- crates/wallets/src/lib.rs
+++ crates/wallets/src/lib.rs
@@ -8,7 +8,6 @@ pub use self::{bip0039::*, lang::*, primitives::*, utils::*};
 
 pub(crate) mod bip0039;
 pub(crate) mod lang;
-pub(crate) mod primitives;
 pub(crate) mod utils;
 
 pub mod assets;
@@ -16,3 +15,20 @@ pub mod authorize;
 pub mod credentials;
 pub mod mnemonics;
 pub mod wallets;
+
+pub(crate) mod primitives {
+    use secp256k1::{PublicKey, SecretKey};
+
+    /// Define the valid sizes of generated access grants
+    pub const ACCESS_GRANT_VALID_BIT_SIZES: [usize; 5] = [128, 160, 192, 224, 256];
+    /// Define the default filepath for locating the BIP0039 english text file
+    pub const PATH_TO_BIP0039_DATA: &str = "**/BIP0039/english.txt";
+    /// Define the endpoint pointing to BIP0039 Mnemonics
+    pub const BIP0039_WORDLIST_ENDPOINT: &str =
+        "https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039";
+
+    
+
+    /// Type alias for a tuple ([secp256k1::SecretKey], [secp256k1::PublicKey])
+    pub type SecpKeypair = (SecretKey, PublicKey);
+}
