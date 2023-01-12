77e2efcd753348f0223814959d23e814485e2fa5
Joe McCain III <jo3mccain@icloud.com>
2023-01-12 22:11:53 +0000

    Update

--- pzzld/src/app.rs
+++ pzzld/src/app.rs
@@ -25,14 +25,12 @@ pub fn app(cx: Scope) -> Element {
                     div { class: "lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center",
                         h1 { class: "title-font sm:text-4xl text-3xl mb-4 font-medium text-white",
                             br { class: "hidden lg:inline-block" }
-                            "Dioxus Sneak Peek"
+                            "Puzzled"
                         }
                         p {
                             class: "mb-8 leading-relaxed",
 
-                            "Dioxus is a new UI framework that makes it easy and simple to write cross-platform apps using web
-                            technologies! It is functional, fast, and portable. Dioxus can run on the web, on the desktop, and
-                            on mobile and embedded platforms."
+                            "Welcome to Puzzled, a "
 
                         }
                         div { class: "flex justify-center",
